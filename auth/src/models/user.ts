45be5c0b7697623ba6d01ea68a445bded2b14425
vladilin <ilin96inbox@gmail.com>
2020-06-01 20:40:51 +0900

    formationg user json

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -15,16 +15,28 @@ interface UserDoc extends mongoose.Document {
   passwords: string;
 }
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+        delete ret.__v;
+      },
+    },
+  }
+);
 
 // hash password before sign up
 // not arrow function to save reference from UserDoc

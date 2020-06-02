9b5be88e9b83a8e7e1eda8aeb6cdfa8aea12b7ae
vladilin <ilin96inbox@gmail.com>
2020-06-02 11:32:21 +0900

    global signin function

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -2,14 +2,7 @@ import request from "supertest";
 import { app } from "../../app";
 
 it("responds with current user", async () => {
-  const authResponse = await request(app)
-    .post("/api/users/signup")
-    .send({
-      email: "test@tes.com",
-      password: "password",
-    })
-    .expect(201);
-  const cookie = authResponse.get("Set-Cookie");
+  const cookie = await global.signin();
 
   const response = await request(app)
     .get("/api/users/currentuser")
@@ -17,5 +10,5 @@ it("responds with current user", async () => {
     .send()
     .expect(200);
 
-  expect(response.body.currentUser.email).toEqual("test@tes.com");
+  expect(response.body.currentUser.email).toEqual("test@test.com");
 });

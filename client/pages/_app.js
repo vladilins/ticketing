7addb479f8a03f43a295f45c4cccacd8184e8b34
vladilin <ilin96inbox@gmail.com>
2020-06-03 20:47:23 +0900

    header

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,30 @@
 import "bootstrap/dist/css/bootstrap.css";
+import buildClient from "../api/build-client";
+import Header from "../components/header";
 
-export default ({ Component, pageProps }) => {
-  return <Component {...pageProps}></Component>;
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <Header currentUser={currentUser}></Header>
+      <Component {...pageProps}></Component>
+    </div>
+  );
 };
+
+AppComponent.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get("/api/users/currentuser");
+  let pageProps = {};
+  // invoke getInitialProps manually because it doesn't work automatically
+  // if we use it in AppComponent
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  return {
+    pageProps,
+    ...data,
+  };
+};
+
+export default AppComponent;

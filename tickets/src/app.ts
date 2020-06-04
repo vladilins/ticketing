d627e9d83b309b03dfd4dd7659dde9722934866a
vladilin <ilin96inbox@gmail.com>
2020-06-04 20:18:23 +0900

    auth

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,7 +2,8 @@ import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
 import cookieSession from "cookie-session";
-import { errorHandler, NotFoundError } from "@vtickets/common";
+import { errorHandler, NotFoundError, currentUser } from "@vtickets/common";
+import { CreateTicketRouter } from "./routes/new";
 
 const app = express();
 app.set("trust proxy", true);
@@ -14,6 +15,9 @@ app.use(
     secure: process.env.NODE_ENV !== "test",
   })
 );
+app.use(currentUser);
+
+app.use(CreateTicketRouter);
 
 app.all("*", async () => {
   throw new NotFoundError();
